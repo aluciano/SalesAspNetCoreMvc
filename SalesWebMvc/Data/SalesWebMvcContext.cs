59e3c0b03cff365a94dce25de490a37284c024dc
Anderson Luciano <aluciano.net@gmail.com>
2019-07-15 19:26:14 -0300

    Adicionando o CRUD completo de Department via scafolding já com Entity Framework Core. Criada a primeira migration e gerado o banco de dados inicial.

---
+++ SalesWebMvc/Data/SalesWebMvcContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SalesWebMvc.Models
+{
+    public class SalesWebMvcContext : DbContext
+    {
+        public SalesWebMvcContext (DbContextOptions<SalesWebMvcContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Department> Department { get; set; }
+    }
+}
